4385064b634cee4f86b3c4e108641ccf4131e2e9
Yibo Guo <nil.yibo@gmail.com>
2019-04-14 11:03:43 -0700

    Added mutex around logging calls.

--- ffrouter/log.h
+++ ffrouter/log.h
@@ -5,12 +5,16 @@
 #define LOG_H
 
 #include <iostream>
+#include <mutex>
 
-#define LOG_LEVEL 10
+#define LOG_LEVEL 2
 
-#define LOG_INFO(x) (((LOG_LEVEL) <= 3) ? std::cout << "[INFO] "  << x << std::endl : std::cout << "")
-#define LOG_DEBUG(x) (((LOG_LEVEL) <= 2) ? std::cout << "[DEBUG] " << x << std::endl : std::cout << "")
-#define LOG_TRACE(x) (((LOG_LEVEL) <= 1) ? std::cout << "[TRACE] " << x << std::endl : std::cout << "")
-#define LOG_ERROR(x) (std::cerr << "[ERROR] " << x << std::endl);
+static std::mutex lock;
+#define LOG_MSG(tag, message) {lock.lock(); std::cout << tag << message << std::endl; lock.unlock();}
+
+#define LOG_INFO(x)  {if ((LOG_LEVEL) <= 3) LOG_MSG("[INFO] ",  x);}
+#define LOG_DEBUG(x) {if ((LOG_LEVEL) <= 2) LOG_MSG("[DEBUG] ", x);}
+#define LOG_TRACE(x) {if ((LOG_LEVEL) <= 1) LOG_MSG("[TRACE] ", x);}
+#define LOG_ERROR(x) {LOG_MSG("[ERROR] ", x);}
 
 #endif /* LOG_H */
